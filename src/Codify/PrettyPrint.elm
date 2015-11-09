7ee63354792dd3f38a06a1f274d50cdcb8d2851e
Richard Feldman <richard.t.feldman@gmail.com>
2015-11-09 22:42:25 +0100

    Make PrettyPrint.typeAlias work

--- src/Codify/PrettyPrint.elm
+++ src/Codify/PrettyPrint.elm
@@ -1,109 +1,114 @@
-module Codify.PrettyPrint (typeToString) where
+module Codify.PrettyPrint (typeAlias, typeToString) where
 
 import Codify exposing (Type(..))
 
---import String
---import Dict exposing (Dict)
+import String
+import Dict exposing (Dict)
 
 
---typeAlias : String -> Int -> Type -> String
---typeAlias str indentSpaces typeValue =
---    "type alias " ++ str ++ " =\n" ++ (typeAliasHelp 1 indentSpaces typeValue)
-
---typeAliasHelp : Int -> Int -> Type -> String
---typeAliasHelp indentLevel indentSpaces typeValue =
---    let
---        indentStr =
---            String.repeat (indentSpaces * indentLevel) " "
-
---        subIndentStr =
---            indentStr ++ String.repeat (indentSpaces) " "
-
---        str =
---            case typeValue of
---                IntType ->
---                    toString intVal
-
---                FloatType ->
---                    toString floatVal
-
---                StringType ->
---                    toString str
-
---                TupleType list ->
---                    if List.isEmpty list then
---                        "[]"
---                    else
---                        let
---                            listBody =
---                                list
---                                    |> List.map (typeAliasHelp 0 indentSpaces)
---                                    |> String.join ("\n" ++ indentStr ++ ", ")
---                        in
---                            "[ " ++ listBody ++ "\n" ++ indentStr ++ "]"
-
---                RecordType dict ->
---                    if Dict.isEmpty dict then
---                        "{}"
---                    else
---                        let
---                            printTypedField (field, fieldType) =
---                                field
---                                    ++ " : "
---                                    ++ (typeAliasHelp 0 indentSpaces fieldType)
-
---                            --type alias Foo =
---                            --    { bar : 12.3
---                            --    , baz : \"baz\"
---                            --    , foo : 1
---                            --bar : 12.3
---                            --    , baz : \"baz\"
---                            --    , foo : 1}
+typeAlias : String -> Type -> String
+typeAlias str typeValue =
+    "type alias " ++ str ++ " =\n" ++ (typeToString False typeValue)
 
 
---                            dictBody =
---                                Debug.log "DICT IS" dict
---                                    |> Dict.toList
---                                    |> List.map printTypedField
---                                    |> String.join ("\n" ++ indentStr ++ ", ")
---                        in
---                            "{ " ++ dictBody ++ "\n" ++ dictBody ++ "}"
---                NullType ->
---                    "null"
---                UndefinedType ->
---                    "undefined"
---    in
---        indentStr ++ str
+fieldPairToString : (String, Type) -> String
+fieldPairToString (field, fieldType) =
+    field ++ " :" ++ (nestedTypeToString fieldType)
+
+
+nestedTypeToString : Type -> String
+nestedTypeToString nestedType =
+    case nestedType of
+        RecordType _ ->
+            "\n" ++ (indent (typeToString False nestedType))
+
+        TupleType _ ->
+            "\n" ++ (indent (typeToString False nestedType))
+
+        _ ->
+            " " ++ (typeToString False nestedType)
+
+
+indentSpaces : Int
+indentSpaces =
+    4
+
+
+indentation : String
+indentation =
+    String.repeat indentSpaces " "
+
+
+indent : String -> String
+indent str =
+    str
+        |> String.split "\n"
+        |> List.map (\line -> indentation ++ line)
+        |> String.join "\n"
+
+
+multilineType : String -> String -> String -> List String -> String
+multilineType openingDelimiter closingDelimiter elemDelimiter lines =
+    let
+        header =
+            indentation ++ openingDelimiter ++ " "
+
+        body =
+            String.join ("\n" ++ indentation ++ elemDelimiter ++ " ") lines
+
+        footer =
+            "\n" ++ indentation ++ closingDelimiter
+    in
+        header ++ body ++ footer
 
 
 typeToString : Bool -> Type -> String
 typeToString needsParens typeValue =
     let
-        (supportsParens, baseResult) =
+        ( supportsParens, baseResult ) =
             case typeValue of
                 AliasType aliasName ->
-                    (False, aliasName)
+                    ( False, aliasName )
 
                 IntType ->
-                    (False, "Int")
+                    ( False, "Int" )
 
                 FloatType ->
-                    (False, "Float")
+                    ( False, "Float" )
 
                 StringType ->
-                    (False, "String")
+                    ( False, "String" )
 
                 ListType typeParam ->
-                    (True, "List " ++ (typeToString True typeParam))
+                    ( True, "List " ++ (typeToString True typeParam) )
 
                 MaybeType typeParam ->
-                    (True, "Maybe " ++ (typeToString True typeParam))
+                    ( True, "Maybe " ++ (typeToString True typeParam) )
 
                 RecordType typeDict ->
-                    (True, "{ TODO }")
+                    if Dict.isEmpty typeDict then
+                        ( False, "{}" )
+                    else
+                        let
+                            str =
+                                typeDict
+                                    |> Dict.toList
+                                    |> List.map fieldPairToString
+                                    |> multilineType "{" "}" ","
+                        in
+                            ( True, str )
 
                 TupleType typeParams ->
-                    (True, "( TODO )")
+                    if List.isEmpty typeParams then
+                        ( False, "()" )
+                    else
+                        let
+                            str =
+                                typeParams
+                                    |> List.map nestedTypeToString
+                                    |> multilineType "(" ")" ","
+                        in
+                            ( True, str )
     in
         if needsParens && supportsParens then
             "(" ++ baseResult ++ ")"
