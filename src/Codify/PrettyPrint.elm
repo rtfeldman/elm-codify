e90110a9edd6f5c499ce853105393082441f6ee7
Richard Feldman <richard.t.feldman@gmail.com>
2015-11-10 00:03:17 +0100

    Add support for generating encoders.

--- src/Codify/PrettyPrint.elm
+++ src/Codify/PrettyPrint.elm
@@ -11,22 +11,112 @@ typeAlias str typeValue =
     "type alias " ++ str ++ " =\n" ++ (typeToString False typeValue)
 
 
+encoder : String -> Type -> String
+encoder name typeValue =
+    let
+        annotation =
+            [ name, ":", typeToString False typeValue, "->", "Value" ]
+                |> String.join " "
+
+        declaration =
+            name ++ " ="
+
+        body =
+            toEncoderString False typeValue
+    in
+        [ annotation, declaration, (indent body) ]
+            |> String.join "\n"
+
+
+{-| Example: ( "name", StringType ) ---> "( \"name\", .name >> string )" -}
+fieldPairToEncoderString : ( String, Type ) -> String
+fieldPairToEncoderString ( field, typeValue ) =
+    String.join " "
+        [ "("
+        , "\"" ++ field ++ "\","
+        , "." ++ field
+        , ">>"
+        , (toEncoderString True typeValue)
+        , ")"
+        ]
+
+
+toEncoderString : Bool -> Type -> String
+toEncoderString needsParens typeValue =
+    withParens needsParens
+        <| case typeValue of
+            AliasType aliasName ->
+                ( False, "<< TODO handle alias >>" )
+
+            IntType ->
+                ( False, "int" )
+
+            FloatType ->
+                ( False, "float" )
+
+            BoolType ->
+                ( False, "bool" )
+
+            StringType ->
+                ( False, "string" )
+
+            MaybeType typeParam ->
+                ( True, "Maybe.map " ++ (toEncoderString True typeParam) ++ " >> Maybe.withDefault null" )
+
+            ListType typeParam ->
+                ( True, "list << (List.map " ++ (toEncoderString True typeParam) ++ ")")
+
+            RecordType typeDict ->
+                if Dict.isEmpty typeDict then
+                    ( True, "object []" )
+                else
+                    let
+                        body =
+                            typeDict
+                                |> Dict.toList
+                                |> List.map fieldPairToEncoderString
+                                |> multiline "[" "," "]"
+                    in
+                        ( True, "record\n" ++ (indent body) )
+
+            TupleType typeParams ->
+                if List.isEmpty typeParams then
+                    ( True, "list []" )
+                else
+                    let
+                        body =
+                            typeParams
+                                |> List.map (nestedTypeToString (toEncoderString False))
+                                |> multiline "[" "," "]"
+                    in
+                        ( True, "list\n" ++ (indent body) )
+
+
+decoder : String -> Type -> String
+decoder str typeValue =
+    "type alias " ++ str ++ " =\n" ++ (typeToString False typeValue)
+
+
 fieldPairToString : (String, Type) -> String
 fieldPairToString (field, fieldType) =
-    field ++ " :" ++ (nestedTypeToString fieldType)
+    field ++ " :" ++ (nestedTypeToString (typeToString False) fieldType)
 
 
-nestedTypeToString : Type -> String
-nestedTypeToString nestedType =
-    case nestedType of
-        RecordType _ ->
-            "\n" ++ (indent (typeToString False nestedType))
+nestedTypeToString : (Type -> String) -> Type -> String
+nestedTypeToString converter nestedType =
+    let
+        converted =
+            converter nestedType
+    in
+        case nestedType of
+            RecordType _ ->
+                "\n" ++ (indent converted)
 
-        TupleType _ ->
-            "\n" ++ (indent (typeToString False nestedType))
+            TupleType _ ->
+                "\n" ++ (indent converted)
 
-        _ ->
-            " " ++ (typeToString False nestedType)
+            _ ->
+                " " ++ converted
 
 
 indentSpaces : Int
@@ -47,8 +137,8 @@ indent str =
         |> String.join "\n"
 
 
-multilineType : String -> String -> String -> List String -> String
-multilineType openingDelimiter closingDelimiter elemDelimiter lines =
+multiline : String -> String -> String -> List String -> String
+multiline openingDelimiter elemDelimiter closingDelimiter lines =
     let
         header =
             indentation ++ openingDelimiter ++ " "
@@ -62,58 +152,59 @@ multilineType openingDelimiter closingDelimiter elemDelimiter lines =
         header ++ body ++ footer
 
 
+withParens : Bool -> ( Bool, String ) -> String
+withParens needsParens ( supportsParens, baseResult ) =
+    if needsParens && supportsParens then
+        "(" ++ baseResult ++ ")"
+    else
+        baseResult
+
 typeToString : Bool -> Type -> String
 typeToString needsParens typeValue =
-    let
-        ( supportsParens, baseResult ) =
-            case typeValue of
-                AliasType aliasName ->
-                    ( False, aliasName )
+    withParens needsParens
+        <| case typeValue of
+            AliasType aliasName ->
+                ( False, aliasName )
 
-                IntType ->
-                    ( False, "Int" )
+            IntType ->
+                ( False, "Int" )
 
-                FloatType ->
-                    ( False, "Float" )
+            FloatType ->
+                ( False, "Float" )
 
-                BoolType ->
-                    ( False, "Bool" )
+            BoolType ->
+                ( False, "Bool" )
 
-                StringType ->
-                    ( False, "String" )
+            StringType ->
+                ( False, "String" )
 
-                ListType typeParam ->
-                    ( True, "List " ++ (typeToString True typeParam) )
+            ListType typeParam ->
+                ( True, "List " ++ (typeToString True typeParam) )
 
-                MaybeType typeParam ->
-                    ( True, "Maybe " ++ (typeToString True typeParam) )
+            MaybeType typeParam ->
+                ( True, "Maybe " ++ (typeToString True typeParam) )
 
-                RecordType typeDict ->
-                    if Dict.isEmpty typeDict then
-                        ( False, "{}" )
-                    else
-                        let
-                            str =
-                                typeDict
-                                    |> Dict.toList
-                                    |> List.map fieldPairToString
-                                    |> multilineType "{" "}" ","
-                        in
-                            ( True, str )
+            RecordType typeDict ->
+                if Dict.isEmpty typeDict then
+                    ( False, "{}" )
+                else
+                    let
+                        str =
+                            typeDict
+                                |> Dict.toList
+                                |> List.map fieldPairToString
+                                |> multiline "{" "," "}"
+                    in
+                        ( True, str )
 
-                TupleType typeParams ->
-                    if List.isEmpty typeParams then
-                        ( False, "()" )
-                    else
-                        let
-                            str =
-                                typeParams
-                                    |> List.map nestedTypeToString
-                                    |> multilineType "(" ")" ","
-                        in
-                            ( True, str )
-    in
-        if needsParens && supportsParens then
-            "(" ++ baseResult ++ ")"
-        else
-            baseResult
+            TupleType typeParams ->
+                if List.isEmpty typeParams then
+                    ( False, "()" )
+                else
+                    let
+                        str =
+                            typeParams
+                                |> List.map (nestedTypeToString (typeToString False))
+                                |> multiline "(" "," ")"
+                    in
+                        ( True, str )
